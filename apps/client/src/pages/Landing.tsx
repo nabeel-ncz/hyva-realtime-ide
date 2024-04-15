8ec3092f3728d439d93f78a3f4841585fefba489
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-04-15 17:47:43 +0530

    feat: designed landing page

--- apps/client/src/pages/Landing.tsx
+++ apps/client/src/pages/Landing.tsx
@@ -4,7 +4,10 @@ export default function Landing() {
       <div className="room-gradient-1"></div>
       <div className="room-gradient-2"></div>
       <div className="section-l">
-          
+          <h2>Hyva</h2>
+          <p>Discover the future of collaborative coding with Hyva</p>
+          <p>Effortlessly edit code together in real-time, anytime, anywhere.</p>
+          <button>Start Coding!</button>
       </div>
       <div className="section-r">
         
