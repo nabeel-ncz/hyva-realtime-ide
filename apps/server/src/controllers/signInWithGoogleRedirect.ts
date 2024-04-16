9b7bb19f546f53c79038dfd13d1cdee4c00cfea9
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-04-16 20:28:03 +0530

    fix: issue with sign-in redirection

--- apps/server/src/controllers/signInWithGoogleRedirect.ts
+++ apps/server/src/controllers/signInWithGoogleRedirect.ts
@@ -1,4 +1,4 @@
 import passport from "passport";
 export const signInWithGoogleRedirect = () => {
-    return passport.authenticate('google', { successRedirect: "/oauth2/google/success", failureRedirect: "/oauth2/google/failed" });
+    return passport.authenticate('google', { successRedirect: "/api/oauth2/google/success", failureRedirect: "/api/oauth2/google/failed" });
 }
